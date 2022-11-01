5a17a846ecefc4065dbb34507cac0c086df21820
RAO WAQAS AKRAM <40608449+raowaqasakram@users.noreply.github.com>
2022-11-01 23:56:21 +0500

    Time: 5 ms (30.47%), Space: 42.4 MB (13.77%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack stack = new Stack();
+        Map map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.push(map.get(c));
+            }
+            else if (stack.empty() || (char) stack.pop() != c) return false;
+        }
+        return stack.empty();
+    }
+
+}
